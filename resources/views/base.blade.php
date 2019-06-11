dcc2a1b184284f220dc0d43c1fb2c3ef0c457e1b
dwijpr <dwijpr@gmail.com>
2019-06-11 14:39:17 +0700

    refactor index table of contents

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -3,7 +3,131 @@ $tags = [
   "BelajarMenulis", "Blogging", "Programming",
   "Komputer", "Koding", "Game",
 ];
+
+$posts = [
+  [
+    'Aug 24, 2015',
+    '24/8 2015',
+    'menambah-ukuran-file-upload-pada-aplikasi-berbasis-php-dan-nginx',
+    'Menambah Ukuran File Upload Pada Aplikasi Berbasis PHP dan Nginx',
+  ],
+  [
+    'Aug 23, 2015',
+    '23/8 2015',
+    'error-rvm-install-ruby',
+    'Error RVM Install Ruby',
+  ],
+  [
+    'Aug 22, 2015',
+    '22/8 2015',
+    'mengetahui-ukuran-sebuah-halaman-web',
+    'Mengetahui Ukuran Sebuah Halaman Web Menggunakan Google Chrome',
+  ],
+  [
+    'Aug 21, 2015',
+    '21/8 2015',
+    'template-cv',
+    'Template CV',
+  ],
+  [
+    'Aug 20, 2015',
+    '20/8 2015',
+    'meringkas-javascript-console-log',
+    'Meringkas JavaScript Web console.log',
+  ],
+  [
+    'Aug 19, 2015',
+    '19/8 2015',
+    'breakout-game',
+    'Breakout Game',
+  ],
+  [
+    'Aug 18, 2015',
+    '18/8 2015',
+    'dasar-git',
+    'Dasar Git',
+  ],
+  [
+    'Aug 17, 2015',
+    '17/8 2015',
+    'membuat-rumah-baru-di-ubuntu',
+    'Membuat Rumah Baru di Ubuntu',
+  ],
+  [
+    'Aug 16, 2015',
+    '16/8 2015',
+    'flat-color-picker',
+    'Flat Color Picker',
+  ],
+  [
+    'Aug 15, 2015',
+    '15/8 2015',
+    'terminal-output-menjadi-html-menggunakan-aha',
+    'Terminal Output Menjadi HTML Menggunakan AHA',
+  ],
+  [
+    'Aug 14, 2015',
+    '14/8 2015',
+    'timer-app',
+    'Timer App',
+  ],
+  [
+    'Aug 13, 2015',
+    '13/8 2015',
+    'bermain-tictactoe',
+    'Bermain TicTacToe',
+  ],
+  [
+    'Aug 12, 2015',
+    '12/8 2015',
+    'permainan-tictactoe',
+    'Permainan TicTacToe',
+  ],
+  [
+    'Aug 11, 2015',
+    '11/8 2015',
+    'tahap-demi-tahap-menambahkan-navigation-drawer-membuat-aplikasi-android',
+    'Tahap Demi Tahap Menambahkan Navigation Drawer - Membuat Aplikasi Android',
+  ],
+  [
+    'Aug 10, 2015',
+    '10/8 2015',
+    'menggunakan-redcarpet-pada-jekyll',
+    'Menggunakan RedCarpet Pada Jekyll',
+  ],
+  [
+    'Aug 9, 2015',
+    '9/8 2015',
+    'cara-cepat-memulai-pemrograman-komputer',
+    'Cara Cepat Memulai Pemrograman Komputer',
+  ],
+  [
+    'Jul 30, 2015',
+    '30/7 2015',
+    'script-bash-untuk-integrasi-dengan-git',
+    'Script Bash untuk Integrasi dengan Git',
+  ],
+  [
+    'Jul 28, 2015',
+    '28/7 2015',
+    'mencoba-bit-field',
+    'iMencoba Bit Field',
+  ],
+  [
+    'Jul 17, 2015',
+    '17/7 2015',
+    'menggunakan-text-editor-vim',
+    'Menggunakan Penyunting Teks VIM',
+  ],
+  [
+    'May 27, 2015',
+    '27/5 2015',
+    'mulai-menulis',
+    'Mulai Menulis',
+  ],
+];
 @endphp
+
 <!DOCTYPE html>
 <html lang="id-ID">
   <head>
@@ -92,291 +216,43 @@ $tags = [
           </div>
         </div>
       </div>
-
       <div style="padding-top: 128px;"></div>
+    </div>
 
-        </div>
-        <div id="the-blog">
-            <div class="visible-xs">
-                <header class="site-header">
-    <a href="/">
-        <div class="hidden-xs wrapper">
-            <div class="home">~</div>
-            <div class="site-title">Catatan dwijpr</div>
-            <!--
-            <div class="photoframe">
-                <img src="/old/assets/img/dwi.png" style="padding: 8px;">
+    <div id="the-blog">
+
+      <div class="visible-xs">
+        <header class="site-header">
+          <a href="/">
+            <div class="hidden-xs wrapper">
+              <div class="home">~</div>
+              <div class="site-title">Catatan dwijpr</div>
             </div>
-            -->
-        </div>
-        <div class="visible-xs wrapper">
-            <div class="site-title">Catatan dwijpr</div>
-            <!--
-            <div class="photoframe">
-                <img src="/old/assets/img/dwi.png" style="padding: 8px;">
+            <div class="visible-xs wrapper">
+              <div class="site-title">Catatan dwijpr</div>
             </div>
-            -->
-        </div>
-    </a>
-</header>
+          </a>
+        </header>
+      </div>
 
+      <div class="page-content">
+        <div class="wrapper">
+
+          <div class="toc">
+          @foreach ($posts as $post)
+            <div class="row">
+              <div class="col-sm-3 visible-sm-offset-3 hidden-xs text-info">
+                {{ $post[0] }}
+              </div>
+              <div class="col-xs-4 visible-xs text-info">
+                {{ $post[1] }}
+              </div>
+              <div class="col-xs-8 col-sm-9">
+              <a href="{{ $post[2] }}">{{ $post[3] }}</a>
+              </div>
             </div>
-            <div class="page-content">
-                <div class="wrapper">
-                    <style>
-  .toc{
-    font-size: 13px;
-  }
-  @media (min-width: 768px){
-    .page-content > .wrapper{
-      border: 1px solid transparent;
-    }
-  }
-</style>
-
-<div class="toc">
-
-    <div class="row">
-      <div class="col-sm-3 visible-sm-offset-3 hidden-xs text-info">
-        Aug 24, 2015
-      </div>
-      <div class="col-xs-4 visible-xs text-info">
-        24/8 2015
-      </div>
-      <div class="col-xs-8 col-sm-9">
-            <a href="menambah-ukuran-file-upload-pada-aplikasi-berbasis-php-dan-nginx.html">Menambah Ukuran File Upload Pada Aplikasi Berbasis PHP dan Nginx</a>
-      </div>
-    </div>
-
-    <div class="row">
-      <div class="col-sm-3 visible-sm-offset-3 hidden-xs text-info">
-        Aug 23, 2015
-      </div>
-      <div class="col-xs-4 visible-xs text-info">
-        23/8 2015
-      </div>
-      <div class="col-xs-8 col-sm-9">
-            <a href="error-rvm-install-ruby.html">Error RVM Install Ruby</a>
-      </div>
-    </div>
-
-    <div class="row">
-      <div class="col-sm-3 visible-sm-offset-3 hidden-xs text-info">
-        Aug 22, 2015
-      </div>
-      <div class="col-xs-4 visible-xs text-info">
-        22/8 2015
-      </div>
-      <div class="col-xs-8 col-sm-9">
-            <a href="mengetahui-ukuran-sebuah-halaman-web.html">Mengetahui Ukuran Sebuah Halaman Web Menggunakan Google Chrome</a>
-      </div>
-    </div>
-
-    <div class="row">
-      <div class="col-sm-3 visible-sm-offset-3 hidden-xs text-info">
-        Aug 21, 2015
-      </div>
-      <div class="col-xs-4 visible-xs text-info">
-        21/8 2015
-      </div>
-      <div class="col-xs-8 col-sm-9">
-            <a href="template-cv.html">Template CV</a>
-      </div>
-    </div>
-
-    <div class="row">
-      <div class="col-sm-3 visible-sm-offset-3 hidden-xs text-info">
-        Aug 20, 2015
-      </div>
-      <div class="col-xs-4 visible-xs text-info">
-        20/8 2015
-      </div>
-      <div class="col-xs-8 col-sm-9">
-            <a href="meringkas-javascript-console-log.html">Meringkas JavaScript Web console.log</a>
-      </div>
-    </div>
-
-    <div class="row">
-      <div class="col-sm-3 visible-sm-offset-3 hidden-xs text-info">
-        Aug 19, 2015
-      </div>
-      <div class="col-xs-4 visible-xs text-info">
-        19/8 2015
-      </div>
-      <div class="col-xs-8 col-sm-9">
-            <a href="breakout-game.html">Breakout Game</a>
-      </div>
-    </div>
-
-    <div class="row">
-      <div class="col-sm-3 visible-sm-offset-3 hidden-xs text-info">
-        Aug 18, 2015
-      </div>
-      <div class="col-xs-4 visible-xs text-info">
-        18/8 2015
-      </div>
-      <div class="col-xs-8 col-sm-9">
-            <a href="dasar-git.html">Dasar Git</a>
-      </div>
-    </div>
-
-    <div class="row">
-      <div class="col-sm-3 visible-sm-offset-3 hidden-xs text-info">
-        Aug 17, 2015
-      </div>
-      <div class="col-xs-4 visible-xs text-info">
-        17/8 2015
-      </div>
-      <div class="col-xs-8 col-sm-9">
-            <a href="membuat-rumah-baru-di-ubuntu.html">Membuat Rumah Baru di Ubuntu</a>
-      </div>
-    </div>
-
-    <div class="row">
-      <div class="col-sm-3 visible-sm-offset-3 hidden-xs text-info">
-        Aug 16, 2015
-      </div>
-      <div class="col-xs-4 visible-xs text-info">
-        16/8 2015
-      </div>
-      <div class="col-xs-8 col-sm-9">
-            <a href="flat-color-picker.html">Flat Color Picker</a>
-      </div>
-    </div>
-
-    <div class="row">
-      <div class="col-sm-3 visible-sm-offset-3 hidden-xs text-info">
-        Aug 15, 2015
-      </div>
-      <div class="col-xs-4 visible-xs text-info">
-        15/8 2015
-      </div>
-      <div class="col-xs-8 col-sm-9">
-            <a href="terminal-output-menjadi-html-menggunakan-aha.html">Terminal Output Menjadi HTML Menggunakan AHA</a>
-      </div>
-    </div>
-
-    <div class="row">
-      <div class="col-sm-3 visible-sm-offset-3 hidden-xs text-info">
-        Aug 14, 2015
-      </div>
-      <div class="col-xs-4 visible-xs text-info">
-        14/8 2015
-      </div>
-      <div class="col-xs-8 col-sm-9">
-            <a href="timer-app.html">Timer App</a>
-      </div>
-    </div>
-
-    <div class="row">
-      <div class="col-sm-3 visible-sm-offset-3 hidden-xs text-info">
-        Aug 13, 2015
-      </div>
-      <div class="col-xs-4 visible-xs text-info">
-        13/8 2015
-      </div>
-      <div class="col-xs-8 col-sm-9">
-            <a href="bermain-tictactoe.html">Bermain TicTacToe</a>
-      </div>
-    </div>
-
-    <div class="row">
-      <div class="col-sm-3 visible-sm-offset-3 hidden-xs text-info">
-        Aug 12, 2015
-      </div>
-      <div class="col-xs-4 visible-xs text-info">
-        12/8 2015
-      </div>
-      <div class="col-xs-8 col-sm-9">
-            <a href="permainan-tictactoe.html">Permainan TicTacToe</a>
-      </div>
-    </div>
-
-    <div class="row">
-      <div class="col-sm-3 visible-sm-offset-3 hidden-xs text-info">
-        Aug 11, 2015
-      </div>
-      <div class="col-xs-4 visible-xs text-info">
-        11/8 2015
-      </div>
-      <div class="col-xs-8 col-sm-9">
-            <a href="tahap-demi-tahap-menambahkan-navigation-drawer-membuat-aplikasi-android.html">Tahap Demi Tahap Menambahkan Navigation Drawer - Membuat Aplikasi Android</a>
-      </div>
-    </div>
-
-    <div class="row">
-      <div class="col-sm-3 visible-sm-offset-3 hidden-xs text-info">
-        Aug 10, 2015
-      </div>
-      <div class="col-xs-4 visible-xs text-info">
-        10/8 2015
-      </div>
-      <div class="col-xs-8 col-sm-9">
-            <a href="menggunakan-redcarpet-pada-jekyll.html">Menggunakan RedCarpet Pada Jekyll</a>
-      </div>
-    </div>
-
-    <div class="row">
-      <div class="col-sm-3 visible-sm-offset-3 hidden-xs text-info">
-        Aug 9, 2015
-      </div>
-      <div class="col-xs-4 visible-xs text-info">
-        9/8 2015
-      </div>
-      <div class="col-xs-8 col-sm-9">
-            <a href="cara-cepat-memulai-pemrograman-komputer.html">Cara Cepat Memulai Pemrograman Komputer</a>
-      </div>
-    </div>
-
-    <div class="row">
-      <div class="col-sm-3 visible-sm-offset-3 hidden-xs text-info">
-        Jul 30, 2015
-      </div>
-      <div class="col-xs-4 visible-xs text-info">
-        30/7 2015
-      </div>
-      <div class="col-xs-8 col-sm-9">
-            <a href="script-bash-untuk-integrasi-dengan-git.html">Script Bash untuk Integrasi dengan Git</a>
-      </div>
-    </div>
-
-    <div class="row">
-      <div class="col-sm-3 visible-sm-offset-3 hidden-xs text-info">
-        Jul 28, 2015
-      </div>
-      <div class="col-xs-4 visible-xs text-info">
-        28/7 2015
-      </div>
-      <div class="col-xs-8 col-sm-9">
-            <a href="mencoba-bit-field.html">Mencoba Bit Field</a>
-      </div>
-    </div>
-
-    <div class="row">
-      <div class="col-sm-3 visible-sm-offset-3 hidden-xs text-info">
-        Jul 17, 2015
-      </div>
-      <div class="col-xs-4 visible-xs text-info">
-        17/7 2015
-      </div>
-      <div class="col-xs-8 col-sm-9">
-            <a href="menggunakan-text-editor-vim.html">Menggunakan Penyunting Teks VIM</a>
-      </div>
-    </div>
-
-    <div class="row">
-      <div class="col-sm-3 visible-sm-offset-3 hidden-xs text-info">
-        May 27, 2015
-      </div>
-      <div class="col-xs-4 visible-xs text-info">
-        27/5 2015
-      </div>
-      <div class="col-xs-8 col-sm-9">
-            <a href="mulai-menulis.html">Mulai Menulis</a>
-      </div>
-    </div>
-
-</div>
+          @endforeach
+          </div>
 
 <p>berlangganan <a href="feed.xml">via RSS</a></p>
 
